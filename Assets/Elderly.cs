8a2660df0b25aef2b056b095f4b6a4b24f966fa5
jellebooij <jellebooij@outlook.com>
2019-01-25 20:19:07 +0100

    AI version 1

---
+++ Assets/Elderly.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Elderly : MonoBehaviour {
+
+    public WaypointCollection waypoints;
+    public float speed;
+
+    private NavMeshAgent nav;
+    private Transform targetWaypoint;
+
+	void Start () {
+
+        nav = GetComponent<NavMeshAgent>();
+        targetWaypoint = waypoints.waypoints[Random.Range(0, waypoints.waypoints.Length - 1)];
+        nav.speed = speed;
+
+    }
+	
+
+	void Update () {
+
+        if(Vector2.Distance(new Vector2(transform.position.x,transform.position.z),new Vector2(targetWaypoint.position.x,targetWaypoint.position.z)) < 0.3f)
+        {
+            targetWaypoint = waypoints.waypoints[Random.Range(0, waypoints.waypoints.Length - 1)];
+        }
+
+        nav.destination = targetWaypoint.position;
+		
+	}
+}
---
+++ Assets/WaypointCollection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointCollection : MonoBehaviour {
+
+    //[HideInInspector]
+    public Transform[] waypoints;
+
+    private void Awake()
+    {
+        waypoints = transform.GetComponentsInChildren<Transform>();
+    }
+
+}
