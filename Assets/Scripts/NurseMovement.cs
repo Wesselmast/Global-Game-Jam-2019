2f4b68f875bf7a33a248ab685730b76d89f4f458
Wessel Mast <mastwessel@gmail.com>
2019-01-25 21:03:56 +0100

    CharacterController af

--- Assets/Scripts/NurseMovement.cs
+++ Assets/Scripts/NurseMovement.cs
@@ -8,14 +8,20 @@ public class NurseMovement : MonoBehaviour {
 
     private NurseInput input;
     private CharacterController controller;
+    private float startY;
 
     private void Awake () {
         input = GetComponent<NurseInput>();
         controller = GetComponent<CharacterController>();
 	}
-	
+
+    private void Start() {
+        startY = transform.position.y;
+    }
+
 	private void FixedUpdate () {
-        Vector3 velocity = new Vector3(input.Horizontal, 0, input.Vertical) * Time.deltaTime * speed;
+        Vector3 velocity = new Vector3(input.Horizontal, 0f, input.Vertical) * Time.deltaTime * speed;
+        transform.position = new Vector3(transform.position.x, startY, transform.position.z);
         controller.Move(velocity);
     }
 }
--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -4,21 +4,34 @@
 public class PlayerCamera : MonoBehaviour {
 
     [SerializeField] private float scrollSpeed;
-    [SerializeField] private float maxDist;
-
-    private GameObject nurse;
+    [SerializeField] private float smooth;
+    [SerializeField] private GameObject nurse;
+    private Vector3 zeroRef = Vector3.zero;
     private NurseInput input;
     private Camera cam;
+    private Vector3 startPos;
     private float travel;
 
     private void Awake () {
-        nurse = transform.parent.gameObject;
         input = nurse.GetComponent<NurseInput>();
         cam = GetComponent<Camera>();
 	}
-	
-	private void FixedUpdate () {
 
+    private void Start() {
+        startPos = transform.position;
+    }
+
+    private void FixedUpdate () {
+        SmoothFollow();
+        Scrolling();
+    }
+
+    private void SmoothFollow() {
+        Vector3 targetPosition = nurse.transform.position + startPos;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref zeroRef, smooth);
+    }
+
+    private void Scrolling() {
         float scrollPos = input.ScrollPos;
         if (scrollPos > 0f && travel > 30) {
             travel = travel - scrollSpeed;
