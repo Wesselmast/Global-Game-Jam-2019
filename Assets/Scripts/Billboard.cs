ab8fa01ec7b2e8746ea65aac8915f7de5696ef2b
EvocateHimself <kevin.absen@hotmail.com>
2019-01-26 10:11:47 +0100

    UI implement push V1

---
+++ Assets/Scripts/Billboard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour {
+
+    Camera mainCamera;
+
+
+    // Initialize the camera as main camera
+    private void Start() {
+        mainCamera = Camera.main;
+    }
+
+
+    // Always face the camera's position and rotation
+    private void Update() {
+        transform.LookAt(transform.position + mainCamera.transform.rotation * Vector3.forward,
+            mainCamera.transform.rotation * Vector3.up);
+    }
+}
---
+++ Assets/Scripts/ChangeMoodEmoji.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeMoodEmoji : MonoBehaviour {
+
+    [SerializeField]
+    private Image meterValue;
+    [SerializeField]
+    private Sprite veryHappy, happy, neutral, sad, verySad;
+	
+	// Update is called once per frame
+	private void Update () {
+        if (meterValue.fillAmount <= 1f && meterValue.fillAmount >= 0.8f) {
+            gameObject.GetComponent<Image>().sprite = veryHappy;
+            meterValue.color = new Color32(139, 195, 74, 255);
+        }
+
+        if (meterValue.fillAmount <= 0.8f && meterValue.fillAmount >= 0.6f) {
+            gameObject.GetComponent<Image>().sprite = happy;
+            meterValue.color = new Color32(203, 215, 84, 255);
+        }
+
+        if (meterValue.fillAmount <= 0.6f && meterValue.fillAmount >= 0.4f) {
+            gameObject.GetComponent<Image>().sprite = neutral;
+            meterValue.color = new Color32(255, 209, 46, 255);
+        }
+
+        if (meterValue.fillAmount <= 0.4f && meterValue.fillAmount >= 0.2f) {
+            gameObject.GetComponent<Image>().sprite = sad;
+            meterValue.color = new Color32(255, 163, 46, 255);
+        }
+
+        if (meterValue.fillAmount <= 0.2f && meterValue.fillAmount >= 0f) {
+            gameObject.GetComponent<Image>().sprite = verySad;
+            meterValue.color = new Color32(255, 76, 76, 255);
+        }
+	}
+}
